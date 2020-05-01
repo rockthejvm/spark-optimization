4bce0a7b6489e2d3fb7afe175b85023f08815f87
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2020-05-01 20:09:17 +0300

    RDD skewed joins

--- src/main/scala/generator/LaptopsDomain.scala
+++ src/main/scala/generator/LaptopsDomain.scala
@@ -2,4 +2,4 @@ package generator
 
 case class LaptopModel(make: String, model: String)
 case class Laptop(registration: String, make: String, model: String, procSpeed: Double)
-  case class LaptopOffer(make: String, model: String, procSpeed: Double, salePrice: Double)
+case class LaptopOffer(make: String, model: String, procSpeed: Double, salePrice: Double)
